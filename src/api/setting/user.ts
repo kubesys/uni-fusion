df252c5aadf635d48e0d4c7e789e92055519fda1
Yanbingshuai <1340660560@qq.com>
2023-07-19 09:58:37 +0800

    提交代码

--- src/api/setting/user.ts
+++ src/api/setting/user.ts
@@ -25,18 +25,18 @@ export function getLogin() {
     return request.get({ url: '/setting/login/detail' })
 }
 
-// export interface LoginSetup {
-//     loginWay: number[] | any // 登录方式, 逗号隔开
-//     forceBindMobile: number // 强制绑定手机 0/1
-//     openAgreement: number // 是否开启协议 0/1
-//     openOtherAuth: number // 第三方登录 0/1
-//     autoLoginAuth: number[] | any // 第三方自动登录 逗号隔开
-// }
+export interface LoginSetup {
+    loginWay: number[] | any // 登录方式, 逗号隔开
+    forceBindMobile: number // 强制绑定手机 0/1
+    openAgreement: number // 是否开启协议 0/1
+    openOtherAuth: number // 第三方登录 0/1
+    autoLoginAuth: number[] | any // 第三方自动登录 逗号隔开
+}
 /**
  * @return { Promise }
  * @param { LoginSetup } LoginSetup
  * @description 设置登录注册规则
  */
-// export function setLogin(params: LoginSetup) {
-//     return request.post({ url: '/setting/login/save', params })
-// }
+export function setLogin(params: LoginSetup) {
+    return request.post({ url: '/setting/login/save', params })
+}

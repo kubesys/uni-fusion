e72afe74ae33d157f08a936ddc3b11ae92ccf488
Yanbingshuai <1340660560@qq.com>
2023-09-25 01:37:17 +0800

    updata

--- src/views/article/CreateJson/CreateJsonDialog.vue
+++ src/views/article/CreateJson/CreateJsonDialog.vue
@@ -1,165 +1,428 @@
 <template>
   <div>
-    <el-dialog
-               width="80%">
-      <div class="create-title">
-        创建{{listname}}
-      </div>
-      <div class="main">
-        <div class="step" style="flex: 0.2;height: 300px" >
-          <el-steps :active="active" direction="vertical">
-            <el-step v-for="(step, stepName) in scaleItems.spec" :key="stepName" :title="stepName" :icon="Edit"/>
-          </el-steps>
+    <el-dialog v-model="dialogVisible" width="60%">
+      <el-scrollbar height="600px">
+        <div class="create-title">
+          创建{{ dialogName }}
         </div>
+        <div class="main">
+          <!-- 原step分步 -->
+          <!--        <div class="step" style="flex: 0.2;height: 300px" >-->
+          <!--          <el-steps :active="active" direction="vertical">-->
+          <!--            <el-step v-for="(step, stepName) in scaleItems.spec" :key="stepName" :title="stepName" :icon="Edit"/>-->
+          <!--          </el-steps>-->
+          <!--        </div>-->
 
-        <div >
-          <div class="card">
-            <el-card v-for="(group, groupName) in currentStepGroups" :key="groupName" style="border: 1px solid #d2d2d2; width: 1000px; margin-top: 10px; margin-left: 30px">
-              <el-form :model="group" :rules="getRules(group)">
-                <el-form-item v-for="(field, fieldName) in group" :key="fieldName" :label="field.value">
-                  <template v-if="field.type === 'textbox'">
-                    <el-input  :placeholder="field.value" type="textarea" :rows="5" />
-                  </template>
-                  <template v-else-if="field.type === 'text'">
-                    <el-input  :placeholder="field.value" />
-                  </template>
-                  <template v-else-if="field.type === 'select'">
-                    <el-select  :placeholder="field.value">
-                      <el-option v-for="(option, optionIndex) in selectOptions" :key="optionIndex" :label="option.label" :value="option.value" />
-                    </el-select>
-                  </template>
-                  <template v-else-if="field.type === 'number'">
-                    <el-input-number  :min="1" :max="10"  />
-                  </template>
-                  <template v-else-if="field.type === 'radio'">
-                    <el-radio-group >
-                      <el-radio :label="3">Option A</el-radio>
-                      <el-radio :label="6">Option B</el-radio>
-                      <el-radio :label="9">Option C</el-radio>
-                    </el-radio-group>
-                  </template>
-                  <template v-else-if="field.type === 'switch'">
-                    <el-switch v-model="value" />
-                  </template>
-                </el-form-item>
-              </el-form>
-            </el-card>
-          </div>
-          <div class="controls__wrap" style="float: right; margin-right: 100px;margin-top: 100px">
-            <div v-if="active != 2 ">
-              <!--      <el-button style="margin-top: 12px" @click="next">下一步</el-button>-->
-              <el-button class="custom-button" @click="handlePrevStep" style="margin-top: 10px;float: right">上一步</el-button>
-              <el-button class="custom-button" @click="handleNextStep" style="margin-top: 10px;float: right;margin-right: 10px">下一步</el-button>
-            </div>
-            <div v-else>
-              <!--      <el-button style="margin-top: 12px" @click="next">完成</el-button>-->
-              <el-button class="custom-button" @click="handlePrevStep" style="margin-top: 10px;float: right">上一步</el-button>
-              <el-button class="custom-button" @click="finishStep" style="margin-top: 10px;float: right;margin-right: 10px">完成</el-button>
-            </div>
+          <div >
+            <div class="card">
+              <!-- 原card 数据 -->
+              <!--            <el-card v-for="(group, groupName) in currentStepGroups" :key="groupName" style="border: 1px solid #d2d2d2; width: 1000px; margin-top: 10px; margin-left: 30px">-->
+              <!--              <el-form :model="group" :rules="getRules(group)">-->
+              <!--                <el-form-item v-for="(field, fieldName) in group" :key="fieldName" :label="field.value">-->
+              <!--                  <template v-if="field.type === 'textbox'">-->
+              <!--                    <el-input  :placeholder="field.value" type="textarea" :rows="5" />-->
+              <!--                  </template>-->
+              <!--                  <template v-else-if="field.type === 'text'">-->
+              <!--                    <el-input  :placeholder="field.value" />-->
+              <!--                  </template>-->
+              <!--                  <template v-else-if="field.type === 'select'">-->
+              <!--                    <el-select  :placeholder="field.value">-->
+              <!--                      <el-option v-for="(option, optionIndex) in selectOptions" :key="optionIndex" :label="option.label" :value="option.value" />-->
+              <!--                    </el-select>-->
+              <!--                  </template>-->
+              <!--                  <template v-else-if="field.type === 'number'">-->
+              <!--                    <el-input-number  :min="1" :max="10"  />-->
+              <!--                  </template>-->
+              <!--                  <template v-else-if="field.type === 'radio'">-->
+              <!--                    <el-radio-group >-->
+              <!--                      <el-radio :label="3">Option A</el-radio>-->
+              <!--                      <el-radio :label="6">Option B</el-radio>-->
+              <!--                      <el-radio :label="9">Option C</el-radio>-->
+              <!--                    </el-radio-group>-->
+              <!--                  </template>-->
+              <!--                  <template v-else-if="field.type === 'switch'">-->
+              <!--                    <el-switch v-model="switchvalue" />-->
+              <!--                  </template>-->
+              <!--                </el-form-item>-->
+              <!--              </el-form>-->
+              <!--            </el-card>-->
 
+              <!--            <pre> {{ formData }} </pre>-->
+              <el-card style="border: 1px solid #d2d2d2; width: 1000px; margin-top: 10px; margin-left: 30px">
+                <el-form :model="formData" ref="formDataRef">
+                  <el-form-item
+                      v-for="(variable, key) in spec.variables"
+                      :label="variable.label"
+                      :key="key"
+                  >
+                    <template v-if="variable.type === 'text' && key.startsWith('metadata')">
+                      <el-input v-model="formData.metadata.name"></el-input>
+                    </template>
+                    <template v-else-if="variable.type === 'text' && key.startsWith('spec')">
+                      <el-input v-model="formData.spec.containers[0].name"></el-input>
+                    </template>
+                    <template v-else-if="variable.type === 'combox'">
+                      <el-select v-model="formData[key]">
+                        <el-option
+                            v-for="option in variable.options"
+                            :key="option.value"
+                            :label="option.label"
+                            :value="option.value"
+                        ></el-option>
+                      </el-select>
+                    </template>
+                    <!-- 处理其他字段类型 -->
+                  </el-form-item>
+                </el-form>
+              </el-card>
+<!--              <pre> {{ jsonFormdata }} </pre>-->
+              <v-ace-editor v-model:value="jsonFormdata"
+                            mode="json"
+                            theme="twilight"
+                            style="height: 300px" />
+            </div>
+            <div class="controls__wrap" style="float: right; margin-right: 100px;margin-top: 100px">
+              <div v-if="active != 2 ">
+                <!--      <el-button style="margin-top: 12px" @click="next">下一步</el-button>-->
+                <el-button class="custom-button" @click="handlePrevStep" style="margin-top: 10px;float: right">上一步</el-button>
+                <el-button class="custom-button" @click="handleNextStep" style="margin-top: 10px;float: right;margin-right: 10px">下一步</el-button>
+              </div>
+              <div v-else>
+                <!--      <el-button style="margin-top: 12px" @click="next">完成</el-button>-->
+                <el-button class="custom-button" @click="handlePrevStep" style="margin-top: 10px;float: right">上一步</el-button>
+                <el-button class="custom-button" @click="finishStep" style="margin-top: 10px;float: right;margin-right: 10px">完成</el-button>
+              </div>
+
+            </div>
           </div>
         </div>
-      </div>
+      </el-scrollbar>
     </el-dialog>
   </div>
 </template>
 <script setup lang="ts">
-import { ref, reactive, onMounted } from 'vue';
-import {Edit} from "@element-plus/icons-vue";
+import { ref, reactive, onMounted, watch } from 'vue';
+// import {Edit} from "@element-plus/icons-vue";
+import { VAceEditor } from "vue3-ace-editor";
 
-defineProps<{CreateJsonVisible:boolean,listname:string}>()
+const switchvalue = ref(true)
+const dialogVisible = ref(false)
+const comboxDrawer = ref(false)
 
-const value = ref(true)
-
-const scaleItems = reactive({
-  spec: {
-    step1: {
-      group1: {
-        'spec.replicas': {
-          value: '命名',
-          required: 'true',
-          type: 'text',
-        },
-        'spec.introduce': {
-          value: '简介',
-          required: 'false',
-          type: 'textbox'
-        },
-        'spec.number': {
-          value: '实例数',
-          required: 'true',
-          type: 'number',
-          regexp: '1<x<10'
-        },
-      },
-      group2: {
-        'spec.replicas': {
-          value: '计算规格',
-          required: 'true',
-          type: 'select'
-        },
-        'spec.radio': {
-          value: '需要',
-          required: 'true',
-          type: 'radio'
-        },
-        'spec.switch': {
-          value: '是否开启',
-          required: 'true',
-          type: 'switch'
-        }
-      }
+const spec = ref({
+  // 你的规格数据
+  "template": {
+    "apiVersion": "v1",
+    "kind": "Pod"
+  },
+  "variables": {
+    "metadata.name": {
+      "label": "名称",
+      "type": "text",
+      "regexp": [
+        "A-Za-z"
+      ]
     },
-    step2: {
-      group1: {
-        'spec.namespace': {
-          value: '命名空间',
-          required: 'true',
-          type: 'text',
-          regexp: '1<x<10'
-        }
-      },
-      group2: {
-        'spec.namespace': {
-          value: 'xxx',
-          required: 'true',
-          type: 'textbox',
-          regexp: '1<x<10'
-        }
-      },
-      group3: {
-        'spec.namespace': {
-          value: 'xxx',
-          required: 'true',
-          type: 'textbox',
-          regexp: '1<x<10'
-        }
-      }
+    "spec.containers[0].name": {
+      "label": "名称",
+      "type": "text",
+      "regexp": [
+        "A-Za-z"
+      ]
     },
-    step3: {
-      group1: {
-        'spec.namespace': {
-          value: '其他',
-          required: 'false',
-          type: 'textbox'
-        }
-      }
+    "spec.containers[0].image": {
+      "label": "镜像",
+      "type": "combox",
+      "kind": "ConfigMap",
+      "name": "busybox"
+    },
+    "spec.containers[0].env": {
+      "label": "环境变量",
+      "required": false,
+      "type": "map",
+      "keyRegexp": [
+        "A-Za-z"
+      ],
+      "valueRegexp": [
+        "A-Za-z"
+      ]
+    },
+    "spec.containers[0].port": {
+      "label": "开放端口",
+      "required": false,
+      "type": "list",
+      "regexp": [
+        "0-9"
+      ]
+    },
+    "spec.containers[0].command": {
+      "label": "启动指令",
+      "required": false,
+      "type": "list",
+      "regexp": [
+        "A-Za-z"
+      ]
+    },
+    "spec.containers[0].args": {
+      "label": "启动参数",
+      "required": false,
+      "type": "list",
+      "regexp": [
+        "A-Za-z"
+      ]
     }
   }
 });
+
+const formData = ref({
+  // 初始化 formData 数据结构
+});
+
+// 处理特殊字段类型，比如 combox 的选项
+// onMounted(() => {
+//   for (const key in spec.value.variables) {
+//     if (spec.value.variables[key].type === 'combox') {
+//       spec.value.variables[key].options = fetchOptionsFromConfigMap();
+//     }
+//   }
+// });
+
+// 这里根据规格数据生成 formData 初始结构，你可以根据需求进行调整
+function generateInitialFormData() {
+  const initialData = {
+    apiVersion: spec.value.template.apiVersion,
+    kind: spec.value.template.kind,
+    metadata: {
+      name: '',
+    },
+    spec: {
+      containers: [
+        {
+          name: '',
+          image: '',
+          port: [
+            {
+              containerPort: 0,
+            },
+          ],
+          env: [],
+          command: [],
+          args: [],
+        },
+      ],
+    },
+  };
+
+  // 这里可以根据 spec.variables 中的内容进一步构建 initialData
+
+  return initialData;
+}
+
+formData.value = generateInitialFormData();
+
+const jsonFormdata = ref(JSON.stringify(formData.value, null, 2));
+
+watch(formData, () => {
+  // 更新 jsonFormdata
+  jsonFormdata.value = JSON.stringify(formData.value, null, 2);
+}, { deep: true });
+
+// const formData = reactive({
+//   apiVersion: 'v1',
+//   kind: 'Pod',
+//   metadata: {
+//     name: '',
+//   },
+//   spec: {
+//     containers: [
+//       {
+//         name: '',
+//         image: '',
+//         ports: [
+//           {
+//             containerPort: 0,
+//           },
+//         ],
+//         env: [],
+//         command: [],
+//         args: [],
+//       },
+//     ],
+//   },
+// });
+
+// const scaleItems = reactive({
+//   spec: {
+//     step1: {
+//       group1: {
+//         'spec.replicas': {
+//           value: '命名',
+//           required: 'true',
+//           type: 'text',
+//         },
+//         'spec.introduce': {
+//           value: '简介',
+//           required: 'false',
+//           type: 'textbox'
+//         },
+//         'spec.number': {
+//           value: '实例数',
+//           required: 'true',
+//           type: 'number',
+//           regexp: '1<x<10'
+//         },
+//       },
+//       group2: {
+//         'spec.replicas': {
+//           value: '计算规格',
+//           required: 'true',
+//           type: 'select'
+//         },
+//         'spec.radio': {
+//           value: '需要',
+//           required: 'true',
+//           type: 'radio'
+//         },
+//         'spec.switch': {
+//           value: '是否开启',
+//           required: 'true',
+//           type: 'switch'
+//         }
+//       }
+//     },
+//     step2: {
+//       group1: {
+//         'spec.namespace': {
+//           value: '命名空间',
+//           required: 'true',
+//           type: 'text',
+//           regexp: '1<x<10'
+//         }
+//       },
+//       group2: {
+//         'spec.namespace': {
+//           value: 'xxx',
+//           required: 'true',
+//           type: 'textbox',
+//           regexp: '1<x<10'
+//         }
+//       },
+//       group3: {
+//         'spec.namespace': {
+//           value: 'xxx',
+//           required: 'true',
+//           type: 'textbox',
+//           regexp: '1<x<10'
+//         }
+//       }
+//     },
+//     step3: {
+//       group1: {
+//         'spec.namespace': {
+//           value: '其他',
+//           required: 'false',
+//           type: 'textbox'
+//         }
+//       }
+//     }
+//   }
+// });
+// const scaleItems = reactive({
+//   spec: {
+//     template: {
+//       apiVersion: 'v1',
+//       kind: 'Pod',
+//     },
+//     variables: {
+//       'metadata.name': {
+//         label: 'metadata名称:',
+//         type: 'text',
+//         regexp: ['A-Za-z'],
+//       },
+//       'spec.containers[0].name': {
+//         label: 'containers名称:',
+//         type: 'text',
+//         regexp: ['A-Za-z'],
+//       },
+//       'spec.containers[0].image': {
+//         label: '镜像:',
+//         type: 'combox',
+//         options: {
+//           'my-image:latest': 'my-image:latest',
+//           // 添加更多选项
+//         },
+//       },
+//       'spec.containers[0].env': {
+//         label: '环境变量:',
+//         required: false,
+//         type: 'map',
+//         keyRegexp: ['A-Za-z'],
+//         valueRegexp: ['A-Za-z'],
+//       },
+//       'spec.containers[0].port': {
+//         label: '开放端口:',
+//         required: false,
+//         type: 'list',
+//         regexp: ['0-9'],
+//       },
+//       'spec.containers[0].command': {
+//         label: '启动指令:',
+//         required: false,
+//         type: 'list',
+//         regexp: ['A-Za-z'],
+//       },
+//       'spec.containers[0].args': {
+//         label: '启动参数:',
+//         required: false,
+//         type: 'list',
+//         regexp: ['A-Za-z'],
+//       },
+//     },
+//   }
+// });
+
+// const submitForm = () => {
+//   // 构建最终的 JSON 对象
+//   const finalJson = {
+//     apiVersion: formData.apiVersion,
+//     kind: formData.kind,
+//     metadata: {
+//       name: formData['metadata.name'],
+//     },
+//     spec: {
+//       containers: [
+//         {
+//           name: formData['spec.containers[0].name'],
+//           image: formData['spec.containers[0].image'],
+//           ports: [
+//             {
+//               containerPort: formData['spec.containers[0].port'],
+//             },
+//           ],
+//           env: formData['spec.containers[0].env'],
+//           command: formData['spec.containers[0].command'],
+//           args: formData['spec.containers[0].args'],
+//         },
+//       ],
+//     },
+//   };
+//
+//   // 打印最终 JSON 对象
+//   console.log(JSON.stringify(finalJson, null, 2));
+// };
 
 const selectOptions = ref([]);
 const currentStep = ref('step1');
 
 // 计算当前步骤对应的 group
-const currentStepGroups = ref({});
-onMounted(() => {
-  updateCurrentStepGroups();
-});
+// const currentStepGroups = ref({});
+// onMounted(() => {
+//   updateCurrentStepGroups();
+// });
 
-function updateCurrentStepGroups() {
-  currentStepGroups.value = scaleItems.spec[currentStep.value];
-}
+// function updateCurrentStepGroups() {
+//   currentStepGroups.value = scaleItems.spec[currentStep.value];
+// }
 
 function getRules(group) {
   // 在这里编写验证规则
@@ -192,21 +455,27 @@ function handlePrevStep() {
   hasNextStep.value = currentIndex < stepNames.length - 2;
 }
 
-function finishStep() {
-  CreateJsonVisible.value = true
+const dialogName = ref('')
+const showAndInit = (listName:any) => {
+  dialogName.value = listName
+  dialogVisible.value = true
 }
+
+defineExpose({
+  showAndInit,
+})
 
 </script>
 <style>
 .create-title{
-  margin-left: 200px;
+  margin-left: 50px;
   margin-top: 50px;
   font-size: 40px;
 }
 
 .main{
-  margin-left: 200px;
-  margin-top: 20px;
+  margin-left: 25px;
+  margin-top: 10px;
   margin-bottom: 30px;
   overflow: hidden;
   display: flex;
@@ -218,7 +487,7 @@ function finishStep() {
 }
 
 .card{
-  height: 400px;
+  height: 100%;
 }
 
 .el-step__title {

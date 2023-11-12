267c9bed6733b43bd25c7d0b89e5096991b778ab
yanbingshuai <1340660560@qq.com>
2023-11-12 20:04:07 +0800

    updata kube-frontend.yaml

---
+++ frontend/install.sh
@@ -0,0 +1,9 @@
+img="frontend"
+version="0.0.3"
+repo="g-ubjg5602-docker.pkg.coding.net/iscas-system/containers"
+
+docker buildx create --name mybuilder --driver docker-container
+docker buildx use mybuilder
+docker run --privileged --rm tonistiigi/binfmt --install all
+
+docker buildx build . --platform linux/arm64,linux/amd64 -t $repo/$img:v$version --push -f Dockerfile
